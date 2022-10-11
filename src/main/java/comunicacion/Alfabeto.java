7dc9a7d95a8c98753b829da8d04c0a73191f056c
Lema25 <56377966+Lema25@users.noreply.github.com>
2022-10-11 08:54:24 -0500

    Add files via upload

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,35 @@
+package comunicacion;
+import java.util.*;
+
+public class Alfabeto extends Pictograma{
+	private String[] letras;
+	private String interpretacion;
+	
+	public Alfabeto(String origen, String[] letras, String interpretacion) {
+		super(origen);
+		this.letras = letras;
+		this.interpretacion = interpretacion;	
+	}
+	
+	public String[] getLetras() {
+		return letras;}
+	public void setLetras(String[] letras) {
+		this.letras = letras;}
+
+	public String getInterpretacion() {
+		return interpretacion;}
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;}
+	
+	public int cantidadLetras() {
+		return letras.length;
+	}
+	
+	public String interpretacion() {
+		return interpretacion;
+	}
+	
+	public String toString() {
+		return Arrays.toString(letras).replace("[", "").replace("]", "");
+	}
+}
